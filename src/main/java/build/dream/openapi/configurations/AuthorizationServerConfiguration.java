acbe2b327cfc7661eb6f89dcfd5fac67c72c363c
liuyandong <554030404@qq.com>
2018-12-27 22:18:49 +0800

    aafaf

--- src/main/java/build/dream/openapi/configurations/AuthorizationServerConfiguration.java
+++ src/main/java/build/dream/openapi/configurations/AuthorizationServerConfiguration.java
@@ -1,52 +1,48 @@
 package build.dream.openapi.configurations;
 
-import build.dream.openapi.auth.CustomAuthorizationCodeServices;
-import build.dream.openapi.auth.CustomClientDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
-import javax.sql.DataSource;
-
 @Configuration
-@EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
     @Autowired
-    private CustomClientDetailsService customClientDetailsService;
+    private AuthenticationManager authenticationManager;
+
     @Autowired
-    private DataSource dataSource;
-
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer oauthServer) {
-        oauthServer.allowFormAuthenticationForClients();
-    }
-
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-        endpoints.authorizationCodeServices(customAuthorizationCodeServices());
-        endpoints.tokenStore(tokenStore()).allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
-    }
-
-    @Bean
-    public CustomAuthorizationCodeServices customAuthorizationCodeServices() {
-        return new CustomAuthorizationCodeServices(dataSource);
-    }
-
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.withClientDetails(customClientDetailsService);
-    }
+    UserDetailsService customUserDetailsService;
 
     @Bean
     public TokenStore tokenStore() {
         return new InMemoryTokenStore();
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("client1")
+                .authorizedGrantTypes("authorization_code", "refresh_token")
+                .scopes("test")//授权范围
+                .secret(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("123456"));
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) {
+        security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore()).userDetailsService(customUserDetailsService);
     }
 }
--- src/main/java/build/dream/openapi/configurations/WebSecurityConfiguration.java
+++ src/main/java/build/dream/openapi/configurations/WebSecurityConfiguration.java
@@ -1,52 +1,34 @@
 package build.dream.openapi.configurations;
 
-import build.dream.openapi.auth.CustomUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private CustomUserDetailsService customUserDetailsService;
 
-    /**
-     * 1\这里记得设置requestMatchers,不拦截需要token验证的url
-     * 不然会优先被这个filter拦截,走用户端的认证而不是token认证
-     * 2\这里记得对oauth的url进行保护,正常是需要登录态才可以
-     */
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().cors().disable()
-                .authorizeRequests().antMatchers("/login/**").permitAll()
-                .and()
-                .formLogin().loginPage("/login/index").permitAll();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserDetailsService);
-    }
-
-    /**
-     * support password grant type
-     *
-     * @return
-     * @throws Exception
-     */
-    @Override
     @Bean
+    @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 
+    @Bean
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/libraries/**");
+    protected UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("admin").password(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("admin")).authorities("USER").build());
+        return manager;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService());
     }
 }
